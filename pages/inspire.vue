e7b24dccc553494f3c03bd9f1fa8d5eaddd80725
Teerapat <teerapat.suteerapongpun@gmail.com>
2019-11-08 20:00:45 +0900

    first commit

---
+++ pages/inspire.vue
@@ -0,0 +1,201 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img
+        src="../assets/img/Asset1.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <blockquote class="blockquote">
+        &#8220;The future of para-rubber industrials&#8221;
+        <footer>
+          <small>
+            <em>&mdash;Weii Tasapon</em>
+          </small>
+        </footer>
+      </blockquote>
+       <v-card
+    class="mx-auto"
+    max-width="434"
+    tile
+  >
+    <v-img
+      height="100%"
+      src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+    >
+      <v-row
+        align="end"
+        class="fill-height"
+      >
+        <v-col
+          align-self="start"
+          class="pa-0"
+          cols="12"
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="164"
+            tile
+          >
+            <v-img src="https://scontent-nrt1-1.xx.fbcdn.net/v/t1.0-9/57006220_2066876600034331_7654576197665292288_o.jpg?_nc_cat=105&_nc_oc=AQl5fr6Y5bLStRNdguHBXYpQDEYm_Go2aTT82XaYJXcRPkbUoCPhv4GLxvddzwaxgMs&_nc_ht=scontent-nrt1-1.xx&oh=206ad86397eed75d2b41511df099cbe1&oe=5E5B8DAA"></v-img>
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="rgba(0, 0, 0, .4)"
+            dark
+          >
+            <v-list-item-content>
+              <v-list-item-title class="title">Pramuankosonyut Tasapon</v-list-item-title>
+              <v-list-item-subtitle>Founder</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
+  <v-card
+    class="mx-auto"
+    max-width="434"
+    tile
+  >
+    <v-img
+      height="100%"
+      src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+    >
+      <v-row
+        align="end"
+        class="fill-height"
+      >
+        <v-col
+          align-self="start"
+          class="pa-0"
+          cols="12"
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="164"
+            tile
+          >
+            <v-img src="https://scontent-nrt1-1.xx.fbcdn.net/v/t1.0-9/46521796_10218203591785600_8282142228952907776_n.jpg?_nc_cat=109&_nc_oc=AQlwyMJkAgebbCi9bHmcwg5ez0D26YBQaoA4oi56jFNRra9pzLcf2ou2FzgwhRGhcuY&_nc_ht=scontent-nrt1-1.xx&oh=6299e14de0703d40948053b3c6e1fb83&oe=5E4C4D16"></v-img>
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="rgba(0, 0, 0, .4)"
+            dark
+          >
+            <v-list-item-content>
+              <v-list-item-title class="title">Panus Sundarapura</v-list-item-title>
+              <v-list-item-subtitle>Co-Founder</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
+   <v-card
+    class="mx-auto"
+    max-width="434"
+    tile
+  >
+    <v-img
+      height="100%"
+      src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+    >
+      <v-row
+        align="end"
+        class="fill-height"
+      >
+        <v-col
+          align-self="start"
+          class="pa-0"
+          cols="12"
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="164"
+            tile
+          >
+            <v-img src="https://scontent-nrt1-1.xx.fbcdn.net/v/t1.0-9/49846455_2103218103047178_1602643703355146240_n.jpg?_nc_cat=101&_nc_oc=AQkbntE3PJcdjpOJ52W7-d8i0Iwh0MVRXS7Tse27z7n2rCLZaoxO5b7hONPMJojfSdA&_nc_ht=scontent-nrt1-1.xx&oh=63e1b98dfbf87ebe177544a3b5a67c8c&oe=5E617FB8"></v-img>
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="rgba(0, 0, 0, .4)"
+            dark
+          >
+            <v-list-item-content>
+              <v-list-item-title class="title">Paniti Achararit</v-list-item-title>
+              <v-list-item-subtitle>Co-Founder</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
+  <v-card
+    class="mx-auto"
+    max-width="434"
+    tile
+  >
+    <v-img
+      height="100%"
+      src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+    >
+      <v-row
+        align="end"
+        class="fill-height"
+      >
+        <v-col
+          align-self="start"
+          class="pa-0"
+          cols="12"
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="164"
+            tile
+          >
+            <v-img src="https://sv1.picz.in.th/images/2019/11/09/gbk3WD.jpg"></v-img>
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="rgba(0, 0, 0, .4)"
+            dark
+          >
+            <v-list-item-content>
+              <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to ="~https://www.facebook.com/"
+          >
+            Teerapat Suteerapongpun
+          </v-btn>
+        </v-card-actions>
+              <v-list-item-subtitle>CEO</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
+      <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/"
+          >
+            Back
+          </v-btn>
+        </v-card-actions>
+    </v-flex>
+  </v-layout>
+</template>
